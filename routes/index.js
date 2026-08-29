21aaf5957d8e5058f8db8c1b55c71083db5a4881
Michael Lee <vlmlee2@gmail.com>
2026-08-29 17:55:30 -0500

    remove unused routes

--- routes/index.js
+++ routes/index.js
@@ -14,38 +14,6 @@ router.get('/getColorNames', async function(req, res, next) {
     res.json(response.colors);
 });
 
-router.post('/convertColor', function(req, res, next) {
-    var colors = {};
-    const inputColor = req.body.hex || req.body.rgb;
-    colors.hex = tinycolor(inputColor).toHexString();
-    colors.rgb = tinycolor(inputColor).toRgbString();
-    colors.softRgb = tinycolor(inputColor).desaturate(5).lighten(5).toRgbString();
-    colors.softHex = tinycolor(inputColor).desaturate(5).lighten(5).toHexString();
-    res.json(colors);
-});
-
-router.get('/choose', function(req, res, next) {
-    const preselectedColors = [
-        '#FF6600',
-        '#18AF90',
-        '#2F9BB7',
-        '#FFD65A',
-        '#FF884D',
-        '#9CABE4',
-        '#DADADA',
-        '#81B9C3',
-        '#B388DD',
-        '#FC7D74'
-    ];
-    res.json(preselectedColors);
-});
-
-router.get('/chooseColor', function(req, res, next) {
-    res.sendFile('color-boxes.hbs', {
-        root: path.join(__dirname, '../views')
-    });
-});
-
 router.get('/random', function(req, res, next) {
     var randomColors = [];
     _.times(10, function() {
